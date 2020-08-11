310d887f9ed8f7c9911cfa09215169c950a8c823
Jessica Jernigan <jernigan.jessica.leigh@gmail.com>
2020-08-11 16:10:09 -0500

    end of 13.2

--- models/User.js
+++ models/User.js
@@ -4,7 +4,12 @@ const bcrypt = require('bcrypt');
 
 
 // create our User model
-class User extends Model { }
+class User extends Model {
+  // set up method to run on instance data (per user) to check password
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
 
 // Define table columns and configuration. initialize the model's data and configuration, passing in two objects as arguments.
 User.init(
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -63,14 +63,18 @@ router.post('/login', (req, res) => {
     }
   }).then(dbUserData => {
     if (!dbUserData) {
-      res.status(400).json({ message: 'No user with that email address!' });
+      res.status(400).json({ message: 'Whoops; there is no user with that email address!' });
       return;
     }
 
-    res.json({ user: dbUserData });
-
     // Verify user
-
+    const validPassword = dbUserData.checkPassword(req.body.password);
+    if (!validPassword) {
+      res.status(400).json({ message: 'Incorrect password!' });
+      return;
+    }
+    
+    res.json({ user: dbUserData, message: 'You are now logged in!' });
   });
 });
 
