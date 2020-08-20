a20e308a08e04891c75ca9895521f195c778cbd1
Jessica Jernigan <jernigan.jessica.leigh@gmail.com>
2020-08-19 19:40:15 -0500

    issue with delete functionality - oops

--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -1,6 +1,13 @@
 async function editFormHandler(event) {
   event.preventDefault();
-  await fetch(`/api/posts/${id}`, {
+
+  const id = window.location.toString().split('/')[
+    window.location.toString().split('/').length - 1
+  ];
+
+  const title = document.querySelector('input[name="post-title"]').value;
+
+  const response = await fetch(`/api/posts/${id}`, {
     method: 'PUT',
     body: JSON.stringify({
       title
@@ -9,6 +16,13 @@ async function editFormHandler(event) {
       'Content-Type': 'application/json'
     }
   });
+
+  if (response.ok) {
+    document.location.replace('/dashboard/');
+  } else {
+    alert(response.statusText);
+  }
 }
+
 
 document.querySelector('.edit-post-form').addEventListener('submit', editFormHandler);
